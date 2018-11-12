2d9821eac00aad665618347aa70dc38a96c1430b
Youé Graillot <youe.graillot@gmail.com>
2018-11-12 18:13:35 +0100

    WIP DEBUG

---
+++ Assets/Scripts/Spawner_DebugGesture.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner_DebugGesture : Spawner
+{
+    public override void SpawnIntParameter(int parameter)
+    {
+        GameObject go = Spawn();
+
+		go.GetComponent<TextMesh>().text = parameter.ToString();
+    }
+}
