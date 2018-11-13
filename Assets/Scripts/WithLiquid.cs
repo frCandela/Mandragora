263283c18b34eaa8ce497ccc62b8b44e93e89f4a
Candela François <candela.francois@gmail.com>
2018-11-13 10:55:31 +0100

    Improving simulation

--- Assets/Scripts/WithLiquid.cs
+++ Assets/Scripts/WithLiquid.cs
@@ -1,35 +1,64 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Cut : MonoBehaviour
+public class WithLiquid : MonoBehaviour
 {
+    [Header("Parameters")]
     [SerializeField] private Mesh insideMesh = null;
     [SerializeField] private Material material = null;
+    [SerializeField,] private float minHeight = 0f;
     [SerializeField,] private float maxHeight = 2.55f;
-    [SerializeField, Range(0, 3f)] private float height = 0;
-    [SerializeField] private Vector3 liquidMeshOffset;
-    [SerializeField] float executionTime;   // Raw perf measurement
-    [SerializeField] private float delay = 0f;
-    [SerializeField] private bool showDebugLines = false;
+    [SerializeField, Range(0, 3f)] private float liquidHeight = 0.5f;
+    [SerializeField] private bool containerClosed = true;
 
+    [Header("Debug & Performance")]    
+    [SerializeField] private float executionDelay = 0f;
+    [SerializeField] private bool showDebugLines = false;
+    [SerializeField] float executionTime;   // Raw perf measurement
+
+    // Liquid mesh references
     private GameObject m_gameObject = null;
     private MeshRenderer m_meshRenderer = null;
     private MeshFilter m_meshFilter = null;
     private Mesh m_mesh = null;
-    private Vector3[] m_baseVertices;   // Vertices of the mesh to cut 
-    private int[] m_baseIndices;        // indices of the mesh to cut 
 
+    // Mesh data
+    private Vector3[] m_baseVertices;       // Vertices of the mesh to cut 
+    private int[] m_baseIndices;            // indices of the mesh to cut 
+    Vector3[] newVertices;                  // Vertices of the final cut mesh  
+    List<int> newIndices = new List<int>(); // Indices of the final cut mesh  
 
+    // Usefull tools
+    Plane cutPlane = new Plane();   // Cut plane
+    Ray ray = new Ray();            // Usefull for plane raycast
 
-    // Use this for initialization
+    // Cutting algorithm
+    int[] cuts = new int[3];            // Indices of vertices cut on a single triangle    
+    int m_additionalVerticesCount = 0;  // Count of additionnal vertices created
+
+    // Matching data structures
+    int nbSets = 0;                                             // Count of connex vertices edges
+    List<HashSet<int>> matchSet = new List<HashSet<int>>();     // List of HashSet for matching vertices 
+    List<List<Vector3>> matchEdges = new List<List<Vector3>>(); // List of Vector3 for saving matched edges
+    List<HashSet<int>> sets = new List<HashSet<int>>();         // List of vertex hash in a connex group
+    List<List<Vector3>> edges = new List<List<Vector3>>();      // List of edges of the same connex group
+
+    // Other
+    private float m_timeSinceLastCall = 0f;    // raw perf measurement
+    Color[] colors = { Color.yellow, Color.green, Color.blue, Color.red, Color.magenta, Color.cyan };// Debug lines colors
+
+    // Init
     void Awake ()
     {
-        m_gameObject = new GameObject("Test");
-        m_gameObject.transform.position = transform.position + liquidMeshOffset;
+        // new GameObject
+        m_gameObject = new GameObject("LiquidMesh");
+        m_gameObject.transform.position = transform.position + minHeight * Vector3.up;
         m_gameObject.transform.parent = transform;
         m_gameObject.transform.localScale = new Vector3(1,1,1);
 
+        // Mesh & utility
         m_mesh = new Mesh();
         m_mesh.name = "liquidMesh";
         m_mesh.MarkDynamic();
@@ -41,6 +70,7 @@ public class Cut : MonoBehaviour
         if (material)
             m_meshRenderer.material = material;
 
+        // Init data 
         for( int i = 0; i < 30; ++i)
         {
             sets.Add( new HashSet<int>());
@@ -50,125 +80,54 @@ public class Cut : MonoBehaviour
         m_baseIndices = insideMesh.GetIndices(0); ;
         m_baseVertices = insideMesh.vertices;
 
-        newVertices = new Vector3[4* m_baseVertices.Length];
+        newVertices = new Vector3[m_baseVertices.Length];
         m_baseVertices.CopyTo(newVertices, 0);
     }
 
-    
-
-    Plane cutPlane = new Plane();   // Cut plane
-    Ray ray = new Ray();            // Usefull for plane raycast
-
-    Vector3[] newVertices;                          // Vertices of the final cut mesh  
-    List<int> newIndices = new List<int>();       // Indices of the final cut mesh  
-
-    int[] cuts = new int[3];    // Indices of vertices cut on a single triangle    
-
-    // Time & perf measurement
-    private float m_timeSinceLastCall = 0f;
-
-    private void OnValidate()
+    private void Start()
     {
-        if(m_gameObject)
-            m_gameObject.transform.position = transform.position + liquidMeshOffset;
+        float h = top.position.y - bot.position.y;
+        liquidHeight = h / 2;
     }
 
     // Update is called once per frame
     void Update ()
     {
+        // Calls Update every executionDelay
         m_timeSinceLastCall += Time.deltaTime;
-        if (m_timeSinceLastCall > delay)
+        if (m_timeSinceLastCall > executionDelay)
         {
             float t = Time.realtimeSinceStartup;
-            UpdateGeometry();
-            UpdateMesh();   
+            if (liquidHeight > 0)
+            {
+                BuildEges();
+                BuildTopFace();
+                UpdateMesh();
+                m_meshRenderer.enabled = true;
+            }
+            else
+                m_meshRenderer.enabled = false;
+
+ 
             executionTime = 1000f * (Time.realtimeSinceStartup - t);
             m_timeSinceLastCall = 0;
         }
     }
 
-    int nbSets = 0;                                             // Count of connex vertices edges
-    List<HashSet<int>> matchSet = new List<HashSet<int>>();     // List of HashSet for grouping edges 
-    List<List<Vector3>> matchEdges = new List<List<Vector3>>(); // List of Vector3 for saving edges
-    List<HashSet<int>> sets = new List<HashSet<int>>();
-    List<List<Vector3>> edges = new List<List<Vector3>>();
-
-    //  Match an edge with the others connex groups of edges
-    void Match( Vector3 proj1, Vector3 proj2)
+    void BuildEges()
     {
-        matchSet.Clear();
-        matchEdges.Clear();
-        for (int setIndex = 0; setIndex < nbSets; ++setIndex)
-        {
-            if (sets[setIndex].Count == 0 || sets[setIndex].Contains(proj1.GetHashCode()) || sets[setIndex].Contains(proj2.GetHashCode()))
-            {
-                matchSet.Add(sets[setIndex]);
-                matchEdges.Add(edges[setIndex]);
-            }                
-        }
+        highestEdge = Vector3.negativeInfinity;
+        /*float h = top.position.y - bot.position.y;
+        if (liquidHeight > h)
+            liquidHeight = h;*/
 
-        if (matchSet.Count == 0)
-        {
-            sets[nbSets].Add(proj1.GetHashCode());
-            sets[nbSets].Add(proj2.GetHashCode());
-            edges[nbSets].Add(proj1);
-            edges[nbSets].Add(proj2);
-            nbSets++;
-        }
-        else
-        {
-            matchSet[0].Add(proj1.GetHashCode());
-            matchSet[0].Add(proj2.GetHashCode());
-            matchEdges[0].Add(proj1);
-            matchEdges[0].Add(proj2);
+        Vector3 botPoint = center.position - Vector3.Distance(center.position,bot.position) * Vector3.up;
+        Vector3 meshSpaceBotPoint = Quaternion.Inverse(transform.rotation) * botPoint;
+        Vector3 normal = Quaternion.Inverse(transform.rotation) * Vector3.up;
+        Vector3 p = meshSpaceBotPoint + normal * liquidHeight;
+        RelativeDebugLine(meshSpaceBotPoint, p, Color.red); 
 
-            for( int i = 1; i < matchSet.Count; ++i)
-            {
-                matchSet[0].UnionWith(matchSet[i]);
-                matchEdges[0].AddRange(matchEdges[i]);
-            }
-            while (matchSet.Count > 1)
-            {
-                List<Vector3> oldEdges = matchEdges[1];
-                HashSet<int> oldSet = matchSet[1];
-                sets.Remove(oldSet);
-                edges.Remove(oldEdges);
-                oldSet.Clear();
-                oldEdges.Clear();
-                sets.Add(oldSet);
-                edges.Add(oldEdges);
-                matchSet.RemoveAt(1);
-                nbSets--;
-            }
-        }
-    }
-
-    // Debug line relative to the Gameobject transform
-
-    Color[] colors = { Color.yellow, Color.green, Color.blue, Color.red, Color.magenta, Color.cyan };// Debug lines colors
-    void RelativeDebugLine( Vector3 start, Vector3 end, Color color)
-    {
-        if(showDebugLines)
-        {
-            Debug.DrawLine(
-            m_gameObject.transform.position + m_gameObject.transform.rotation * start,
-            m_gameObject.transform.position + m_gameObject.transform.rotation * end,
-            color
-            );
-        }
-    }
-
-
-
-    
-
-    // Update the mesh 
-    int m_additionalVerticesCount = 0;
-    void UpdateGeometry()
-    {
-        Vector3 p = Quaternion.Inverse(transform.rotation) * (height * transform.up);
-
-        cutPlane.SetNormalAndPosition(Quaternion.Inverse(transform.rotation) * Vector3.up, p.y * Vector3.up);
+        cutPlane.SetNormalAndPosition(normal, p);
 
         newIndices.Clear();
         float distance;
@@ -185,8 +144,7 @@ public class Cut : MonoBehaviour
             int nbCut = 0;
             for (int j = 0; j < 3; ++j)
             {
-                int index = i * 3 + j;
-
+                int index = i * 3 + j;     
                 if (cutPlane.GetSide(m_baseVertices[m_baseIndices[index]]))
                     cuts[nbCut++] = j;
                 else
@@ -199,6 +157,10 @@ public class Cut : MonoBehaviour
                 newIndices.Add(m_baseIndices[i * 3 + 0]);
                 newIndices.Add(m_baseIndices[i * 3 + 1]);
                 newIndices.Add(m_baseIndices[i * 3 + 2]);
+
+                CheckHighest(m_baseVertices[ m_baseIndices[i * 3 + 0]]);
+                CheckHighest(m_baseVertices[ m_baseIndices[i * 3 + 1]]);
+                CheckHighest(m_baseVertices[ m_baseIndices[i * 3 + 2]]);
             }
             // 1 edge cut
             else if (nbCut == 1)
@@ -223,7 +185,7 @@ public class Cut : MonoBehaviour
                     newIndices.Add(m_baseVertices.Length + m_additionalVerticesCount);
                     newIndices.Add(m_baseIndices[i * 3 + (cuts[0] + 1) % 3]);       //p2
                     newIndices.Add(m_baseIndices[i * 3 + (cuts[0] + 3 - 1) % 3]);   // p3
-                    newVertices[m_baseVertices.Length + m_additionalVerticesCount] = proj1;
+                    AddNewVertice(m_baseVertices.Length + m_additionalVerticesCount, proj1);
                     ++m_additionalVerticesCount;
                 }
 
@@ -238,7 +200,7 @@ public class Cut : MonoBehaviour
 
                     newIndices.Add(m_baseIndices[i * 3 + (cuts[0] + 3 - 1) % 3]);//P3
                     newIndices.Add(m_baseVertices.Length + m_additionalVerticesCount);
-                    newVertices[m_baseVertices.Length + m_additionalVerticesCount] = proj2;
+                    AddNewVertice(m_baseVertices.Length + m_additionalVerticesCount, proj2);
                     ++m_additionalVerticesCount;
 
 
@@ -293,22 +255,27 @@ public class Cut : MonoBehaviour
                     newIndices.Add(m_baseVertices.Length + m_additionalVerticesCount);
                     newIndices.Add(m_baseIndices[i * 3 + cuts[2]]); // P3
                     newIndices.Add(m_baseVertices.Length + m_additionalVerticesCount + 1);
-                } 
+                }
 
                 // Adds projected vertices
-                newVertices[m_baseVertices.Length + m_additionalVerticesCount] = proj1;
+                AddNewVertice(m_baseVertices.Length + m_additionalVerticesCount,proj1);
                 ++m_additionalVerticesCount;
-                newVertices[m_baseVertices.Length + m_additionalVerticesCount] = proj2;
+                AddNewVertice(m_baseVertices.Length + m_additionalVerticesCount, proj2);
                 ++m_additionalVerticesCount;
-
             }
         }
     }
 
-    void UpdateMesh()
+    void CheckHighest(Vector3 point)
     {
-        m_mesh.Clear();
-        // Draw the lines
+        if (point.y > highestEdge.y)
+            highestEdge = point;
+    }
+
+    Vector3 highestEdge = Vector3.negativeInfinity;
+    void BuildTopFace()
+    {  
+        // Draw the topFace
         for (int j = 0; j < edges.Count; ++j)
         {
             Vector3 center = Vector3.zero;
@@ -319,6 +286,8 @@ public class Cut : MonoBehaviour
             RelativeDebugLine(center, center + 0.3f * cutPlane.normal, colors[j % colors.Length]);
             for (int i = 0; i < edges[j].Count / 2; ++i)
             {
+                CheckHighest(edges[j][2 * i]);
+
                 RelativeDebugLine(edges[j][2 * i], edges[j][2 * i + 1], colors[j % colors.Length]);
 
                 Vector3 normal = Vector3.Cross(center - edges[j][2 * i], center - edges[j][2 * i + 1]);
@@ -336,21 +305,127 @@ public class Cut : MonoBehaviour
                     newIndices.Add(m_baseVertices.Length + m_additionalVerticesCount + 2);
                 }
 
-
-                newVertices[m_baseVertices.Length + m_additionalVerticesCount] = edges[j][2 * i];
+                AddNewVertice(m_baseVertices.Length + m_additionalVerticesCount, edges[j][2 * i]);
                 ++m_additionalVerticesCount;
-                newVertices[m_baseVertices.Length + m_additionalVerticesCount] = edges[j][2 * i + 1];
+                AddNewVertice(m_baseVertices.Length + m_additionalVerticesCount, edges[j][2 * i + 1]);
                 ++m_additionalVerticesCount;
-                newVertices[m_baseVertices.Length + m_additionalVerticesCount] = center;
+                AddNewVertice(m_baseVertices.Length + m_additionalVerticesCount, center);
                 ++m_additionalVerticesCount;
-
-
             }
         }
 
+        RelativeDebugLine(highestEdge, highestEdge + Vector3.up, Color.blue);
 
+        //
+        //RelativeDebugLine(highestEdge, highestEdge + cutPlane.normal, Color.red);
+         if ( !containerClosed)
+         {
+             if (highestEdge.y > maxHeight)
+             {               
+                 Vector3 p = Quaternion.Inverse(transform.rotation) * (highestEdge);
+                 RelativeDebugLine(p, p + 2*cutPlane.normal, Color.blue);
+
+                 liquidHeight = Mathf.Max(0f, liquidHeight - speed * Time.deltaTime);
+             }
+         }
+
+    }
+
+    [SerializeField] float speed = 0.01f;
+    public Transform top;
+    public Transform center;
+    public Transform bot;
+
+    void UpdateMesh()
+    {
+        m_mesh.Clear();
         m_mesh.vertices = newVertices;
         m_mesh.SetIndices(newIndices.ToArray(), MeshTopology.Triangles, 0);
         m_mesh.RecalculateNormals();
+    }
+   
+    //  Match an edge with the others connex groups of edges
+    void Match(Vector3 proj1, Vector3 proj2)
+    {
+        matchSet.Clear();
+        matchEdges.Clear();
+        for (int setIndex = 0; setIndex < nbSets; ++setIndex)
+        {
+            if (sets[setIndex].Count == 0 || sets[setIndex].Contains(proj1.GetHashCode()) || sets[setIndex].Contains(proj2.GetHashCode()))
+            {
+                matchSet.Add(sets[setIndex]);
+                matchEdges.Add(edges[setIndex]);
+            }
+        }
+
+        if (matchSet.Count == 0)
+        {
+            sets[nbSets].Add(proj1.GetHashCode());
+            sets[nbSets].Add(proj2.GetHashCode());
+            edges[nbSets].Add(proj1);
+            edges[nbSets].Add(proj2);
+            nbSets++;
+        }
+        else
+        {
+            matchSet[0].Add(proj1.GetHashCode());
+            matchSet[0].Add(proj2.GetHashCode());
+            matchEdges[0].Add(proj1);
+            matchEdges[0].Add(proj2);
+
+            for (int i = 1; i < matchSet.Count; ++i)
+            {
+                matchSet[0].UnionWith(matchSet[i]);
+                matchEdges[0].AddRange(matchEdges[i]);
+            }
+            while (matchSet.Count > 1)
+            {
+                List<Vector3> oldEdges = matchEdges[1];
+                HashSet<int> oldSet = matchSet[1];
+                sets.Remove(oldSet);
+                edges.Remove(oldEdges);
+                oldSet.Clear();
+                oldEdges.Clear();
+                sets.Add(oldSet);
+                edges.Add(oldEdges);
+                matchSet.RemoveAt(1);
+                nbSets--;
+            }
+        }
+    }
+
+    // Add a new vertice to ther newVertices array with error checking
+    void AddNewVertice(int index ,  Vector3 vertice)
+    {
+        try
+        {
+            newVertices[index] = vertice;
+        }
+        catch(IndexOutOfRangeException)
+        {
+            Vector3[] newArray = new Vector3[2 * newVertices.Length];
+            newVertices.CopyTo(newArray, 0);
+            newVertices = newArray;
+            AddNewVertice(index, vertice);
+        }
+    }
+
+    // Draws a debug line relative to the liquidMesh gameobject
+    void RelativeDebugLine(Vector3 start, Vector3 end, Color color)
+    {
+        if (showDebugLines)
+        {
+            Debug.DrawLine(
+            m_gameObject.transform.position + m_gameObject.transform.rotation * start,
+            m_gameObject.transform.position + m_gameObject.transform.rotation * end,
+            color
+            );
+        }
+    }
+
+    private void OnValidate()
+    {
+        if (m_gameObject)
+            m_gameObject.transform.position = transform.position + minHeight * Vector3.up;
     }
 }
