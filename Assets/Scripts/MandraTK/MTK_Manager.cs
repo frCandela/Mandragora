e67e07b6910095e2005b139e2e64520822941d28
Candela François <candela.francois@gmail.com>
2018-11-16 16:08:38 +0100

    SteamVr base setup

---
+++ Assets/Scripts/MandraTK/MTK_Manager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MTK_Manager : MonoBehaviour
+{
+    [HideInInspector] public MTK_Setup activeSetup = null;
+
+    // Use this for initialization
+    void Awake ()
+    {            
+        // Activates the first MTK_Setup in the child hierarchy
+        for (int i = 0; i < transform.childCount; ++i)
+        {
+            MTK_Setup setup = transform.GetChild(i).GetComponent<MTK_Setup>();
+            if ( ! activeSetup && setup)
+            {
+                activeSetup = setup;
+                setup.gameObject.SetActive(true);
+            }
+            else
+                setup.gameObject.SetActive(false);
+        }        
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
---
+++ Assets/Scripts/MandraTK/MTK_Setup.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MTK_Setup : MonoBehaviour
+{
+    public GameObject leftHand = null;
+    public GameObject rightHand = null;
+    public GameObject head = null;
+
+    // Use this for initialization
+    void Awake ()
+    {
+        if (!leftHand)
+            leftHand = new GameObject("leftHand");
+        if (!rightHand)
+            rightHand = new GameObject("rightHand");
+        if (!head)
+            head = new GameObject("head");
+
+
+        leftHand.transform.parent = transform;
+        rightHand.transform.parent = transform;
+        head.transform.parent = transform;
+        Camera.main.transform.parent = head.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
