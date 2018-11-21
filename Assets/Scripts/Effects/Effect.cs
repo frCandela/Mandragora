c015ca4baed2796e30d661a7ece2628f7583477e
Candela François <candela.francois@gmail.com>
2018-11-21 14:42:58 +0100

    Levitation zone

---
+++ Assets/Scripts/Effects/Effect.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Effect : MonoBehaviour
+{
+    protected virtual void Awake()
+    {
+        ApplyEffect();
+    }
+
+    private void OnDestroy()
+    {
+        RemoveEffect();
+    }
+
+    public abstract void ApplyEffect();
+    public abstract void RemoveEffect();
+}
+
---
+++ Assets/Scripts/LevitationZone.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevitationZone : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        LevitationEffect levitating = other.GetComponent<LevitationEffect>();
+        if( ! levitating )
+        {
+            other.gameObject.AddComponent<LevitationEffect>();
+            print("zi");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        LevitationEffect levitating = other.GetComponent<LevitationEffect>();
+        if (levitating)
+        {
+            Destroy(levitating);
+            print("zout");
+        }
+    }
+}
