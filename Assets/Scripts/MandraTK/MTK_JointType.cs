dce563f971df276c79bca8018533523a203aac3a
Youé Graillot <youe.graillot@gmail.com>
2018-11-22 19:14:04 +0100

    Clean JointBreakEvent

--- Assets/Scripts/MandraTK/MTK_JointType.cs
+++ Assets/Scripts/MandraTK/MTK_JointType.cs
@@ -3,11 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
-[System.Serializable]
-public class JointBreakEvent : UnityEvent<MTK_JointType>
-{
-}
-
 public abstract class MTK_JointType : MonoBehaviour
 {
     public UnityEvent onJointBreak;
