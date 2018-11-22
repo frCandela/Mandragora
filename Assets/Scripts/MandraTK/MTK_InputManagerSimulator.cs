0525545cfcc7aaf396a4649b9ac9d36ee9b7d3ee
Youé Graillot <youe.graillot@gmail.com>
2018-11-22 23:48:59 +0100

    Implement Angular & Velocity

--- Assets/Scripts/MandraTK/MTK_InputManagerSimulator.cs
+++ Assets/Scripts/MandraTK/MTK_InputManagerSimulator.cs
@@ -7,11 +7,23 @@ public class MTK_InputManagerSimulator : MTK_InputManager
     // public enum Hand {left, right}
     // [SerializeField] private Hand m_hand;
 
-    public override Vector3 GetAngularVelocity() { return Vector3.zero; }
-    public override Vector3 GetVelocity() { return Vector3.zero; }
+    Quaternion m_lastRot;
+	List<Vector3> m_rotList;
+	float m_magnitude;
+	Vector3 m_axis;
+
+	List<Vector3> m_posList;
+
+    void Awake()
+	{
+		m_rotList = new List<Vector3>(5);
+		m_posList = new List<Vector3>(5);
+		m_lastRot = transform.rotation;
+	}
 
     void Update()
     {
+        // Manage inputs
         if (Input.GetMouseButtonDown(0))
             onInput.Invoke(InputButtons.Grip, true);
         if (Input.GetMouseButtonUp(0))
@@ -26,10 +38,49 @@ public class MTK_InputManagerSimulator : MTK_InputManager
             onInput.Invoke(InputButtons.Pad, true);
         if (Input.GetMouseButtonUp(1))
             onInput.Invoke(InputButtons.Pad, false);
+
+        // Update Angular Velocity
+        Quaternion deltaRotation = transform.rotation * Quaternion.Inverse (m_lastRot);
+		deltaRotation.ToAngleAxis(out m_magnitude, out m_axis);
+		m_rotList.Add((m_axis * m_magnitude));
+
+		if (m_rotList.Count > 4)
+			m_rotList.RemoveAt(0);
+			
+		m_lastRot = transform.rotation;
+
+        // Update Velocity
+        m_posList.Add(transform.position);
+
+        if (m_posList.Count > 4)
+			m_posList.RemoveAt(0);
     }
 
     public override void Haptic(float Time)
     {
         print("bzz bzz");
+    }
+
+    public override Vector3 GetAngularVelocity()
+    {
+        Vector3 angularVelocity = Vector3.zero;
+
+		foreach (Vector3 vel in m_rotList)
+			angularVelocity += vel;
+
+		angularVelocity /= m_rotList.Count;
+		return angularVelocity;
+    }
+
+    public override Vector3 GetVelocity()
+    {
+        Vector3 velocity = Vector3.zero;
+
+        foreach (Vector3 vel in m_posList)
+			velocity += vel;
+            
+		velocity /= m_posList.Count;
+
+        return velocity;
     }
 }
