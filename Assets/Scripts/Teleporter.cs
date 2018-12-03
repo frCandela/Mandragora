88d1b880eda8d6a8dcd4419073f5f98b0066ad73
Youé Graillot <youe.graillot@gmail.com>
2018-12-03 17:33:48 +0100

    Working TP

---
+++ Assets/Scripts/Teleporter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleporter : MonoBehaviour {
+
+	[SerializeField] MTK_Manager m_mtkManager;
+	RaycastHit m_rayHit;
+
+	Vector3 m_targetPosition;
+	
+	void Update ()
+	{
+		Transform origin =	m_mtkManager.activeSetup.head.transform;
+
+		if(Physics.Raycast(origin.position, origin.forward, out m_rayHit, 100, LayerMask.GetMask("TP")))
+		{
+			m_targetPosition = m_rayHit.collider.transform.position;
+		}
+	}
+
+	public void Teleport()
+	{
+		m_mtkManager.transform.position = m_targetPosition;
+	}
+}
