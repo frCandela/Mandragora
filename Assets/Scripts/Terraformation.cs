58c530c3f26675fe051d8f6ff3abb554dc972e96
Candela François <candela.francois@gmail.com>
2018-12-09 15:54:46 +0100

    base scene & code

---
+++ Assets/Scripts/Terraformation.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+public class Terraformation : MonoBehaviour
+{
+    private Mesh m_mesh;
+
+    // Use this for initialization
+    void Awake ()
+    {
+        m_mesh = GetComponent<MeshFilter>().mesh;
+    }
+
+    void Update()
+    {
+
+        Vector3[] vertices = m_mesh.vertices;
+        Vector3[] normals = m_mesh.normals;
+
+        for (var i = 0; i < vertices.Length; i++)
+        {
+            vertices[i] += 0.001f * normals[i] * Mathf.Sin(Time.time);
+        }
+
+        m_mesh.vertices = vertices;
+    }
+}
