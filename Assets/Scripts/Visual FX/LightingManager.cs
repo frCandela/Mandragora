14a27f0885d91834e819a1b99cdff1259035354d
Youé Graillot <youe.graillot@gmail.com>
2019-01-31 14:19:13 +0100

    Fix LightManager

--- Assets/Scripts/Visual FX/LightingManager.cs
+++ Assets/Scripts/Visual FX/LightingManager.cs
@@ -5,21 +5,21 @@ using UnityEngine;
 public class LightingManager : MonoBehaviour {
 
 	[Range(0.0f, 1.0f)] public float sceneUnlitFactor;
-	public GameObject sunGO;
-	public bool isLit = true;
-	public List<Light> lights;
+	//public GameObject sunGO;
+	//public bool isLit = true;
+	//public List<Light> lights;
 	private MeshRenderer sunRenderer;
 
 	// Use this for initialization
 	void Start () {
-		sunRenderer = sunGO.GetComponent<MeshRenderer>();
+		//sunRenderer = sunGO.GetComponent<MeshRenderer>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		Shader.SetGlobalFloat("_ManagerUnlitFactor", sceneUnlitFactor);
 
-		if(!isLit) {
+		/*if(!isLit) {
 			foreach(Light item in lights)
 			{
 				item.enabled = false;
@@ -32,6 +32,6 @@ public class LightingManager : MonoBehaviour {
 		}
 
 		//sunRenderer.material.SetFloat("_Visibility", sunVisibility);
-		sunGO.SetActive(isLit);
+		sunGO.SetActive(isLit);*/
 	}
 }
