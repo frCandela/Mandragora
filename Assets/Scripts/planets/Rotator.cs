c971b7b1790c373e6495000a6c721284d9596bbd
Candela François <candela.francois@gmail.com>
2019-02-08 17:11:58 +0100

    Sun rotation

---
+++ Assets/Scripts/planets/Rotator.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    [SerializeField] private Vector3 axis = Vector3.right;
+    [SerializeField] private float angularSpeed = 1;
+
+    // Update is called once per frame
+    void Update ()
+    {
+        transform.Rotate(angularSpeed * axis * Time.deltaTime);
+	}
+}
