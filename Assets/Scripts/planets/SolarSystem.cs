2d9655954f7e9807854dc586b1ffe250090663d5
Youé Graillot <youe.graillot@gmail.com>
2019-02-12 16:44:34 +0100

    Fix Particles on planet

--- Assets/Scripts/planets/SolarSystem.cs
+++ Assets/Scripts/planets/SolarSystem.cs
@@ -60,10 +60,10 @@ public class SolarSystem : MonoBehaviour
         if(other.attachedRigidbody)
         {
             PlanetEffect effect = other.attachedRigidbody.GetComponent<PlanetEffect>();
+            UpdateTPZone(other.GetComponent<IcoPlanet>(), false);
 
             if (effect)
             {
-                UpdateTPZone(other.GetComponent<IcoPlanet>(), false);
                 m_planetEffectsList.Remove(effect);
                 Destroy(effect);
 
@@ -80,13 +80,13 @@ public class SolarSystem : MonoBehaviour
             {
                 foreach (IcoPlanet pl in m_planetList)
                 {
-                    ParticleSystem.EmissionModule emission = planet.GetComponentInChildren<ParticleSystem>().emission;
+                    ParticleSystem.EmissionModule emission = planet.transform.GetChild(0).GetComponentInChildren<ParticleSystem>().emission;
                     emission.rateOverTime = new ParticleSystem.MinMaxCurve(0);
                 }
 
                 if(enter)
                 {
-                    ParticleSystem.EmissionModule emission = planet.GetComponentInChildren<ParticleSystem>().emission;
+                    ParticleSystem.EmissionModule emission = planet.transform.GetChild(0).GetComponentInChildren<ParticleSystem>().emission;
                     emission.rateOverTime = new ParticleSystem.MinMaxCurve(20);
 
                     m_planetList.Add(planet);
