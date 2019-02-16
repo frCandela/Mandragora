6e48ff3bc247391f1497b0c79da4b763b604d005
Unknown <raoul.desmarest@gmail.com>
2019-02-16 21:32:25 +0100

    update

---
+++ Assets/Scripts/trailer/cameraRotate.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class cameraRotate : MonoBehaviour {
+
+	public float rotateSpeed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(0,rotateSpeed,0, Space.World);
+	}
+}
